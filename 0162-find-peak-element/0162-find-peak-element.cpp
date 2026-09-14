8ba099692639586366ff565516784d71e675546f
aayush2006kr <ayushankitaara@gmail.com>
2026-09-14 11:22:58 +0530

    Time: 0 ms (100%), Space: 12.6 MB (51.91%) - LeetHub

---
+++ 0162-find-peak-element/0162-find-peak-element.cpp
@@ -0,0 +1,22 @@
+class Solution {
+public:
+    int findPeakElement(vector<int>& nums) {
+        int low = 0;
+        int high = nums.size()-1;
+        int res = -1;
+
+        while(high>=low){
+            int guess = (low+high)/2;
+            if (guess == nums.size() - 1 || nums[guess] > nums[guess + 1]) {
+                res = guess;
+                high = guess - 1;
+            }
+            else {
+                low = guess + 1;
+            }  
+            
+            
+        }
+        return res;
+    }
+};
